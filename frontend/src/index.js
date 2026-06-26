d941c86da78fff02057811b778c03076dd27a2a5
HabitaClick <raafagt0@gmail.com>
2026-06-26 23:12:35 +0200

    Add Vercel Analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App';
 
@@ -9,5 +10,6 @@ root.render(
   <>
     <App />
     <SpeedInsights />
+    <Analytics />
   </>
 );
